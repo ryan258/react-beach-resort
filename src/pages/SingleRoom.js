2cbe98feff573cb6394b6cd5f1d87773ce66f7b5
Ryan Johnson <ryan@258webco.com>
2020-04-30 18:14:21 -0500

    Set up the SingleRoom pages and apply routing and context.

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,10 +1,61 @@
 import React, { Component } from "react";
+import defaultBcg from "../images/room-1.jpeg";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
+import { RoomContext } from "../context";
 
 export default class SingleRoom extends Component {
+  // react-router passes the props :)
+  constructor(props) {
+    super(props);
+    console.log(this.props);
+    this.state = {
+      slug: this.props.match.params.slug,
+      defaultBcg,
+    };
+  }
+
+  static contextType = RoomContext;
+
+  // componentDidMount() {}
+
   render() {
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
+    console.log(room);
+    if (!room) {
+      return (
+        <div className="error">
+          <h3>no such room!</h3>
+          <Link to="/rooms" className="btn-primary">
+            Back to More Rooms
+          </Link>
+        </div>
+      );
+    }
+
+    const {
+      name,
+      description,
+      capacity,
+      size,
+      price,
+      extras,
+      breakfast,
+      pets,
+      images,
+    } = room;
+
     return (
       <div>
-        <h1>hello from a single room!</h1>
+        <Hero hero="roomsHero">
+          <Banner title={`${name} room`}>
+            <Link to="/rooms" className="btn-primary">
+              back to rooms
+            </Link>
+          </Banner>
+        </Hero>
       </div>
     );
   }
