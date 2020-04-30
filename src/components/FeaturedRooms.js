7ab81fdb13cfd4b4dd1fb2ee30ecca7d95a8e000
Ryan Johnson <ryan@258webco.com>
2020-04-30 16:07:29 -0500

    Make a data connection to the FeaturedRooms component.

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
+import Title from "./Title";
+
 import { RoomContext } from "../context";
+import Loading from "./Loading";
+import Room from "./Room";
 
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
 
   render() {
-    const { featuredRooms: rooms } = this.context;
-    console.log(rooms);
+    let { featuredRooms: rooms, loading } = this.context;
+    // console.log(rooms);
+    rooms = rooms.map((room) => {
+      return <Room key={room.id} room={room} />;
+    });
 
     return (
       <div>
-        <h1>Featured Rooms!</h1>
+        <section className="featured-rooms">
+          <Title title="featured rooms" />
+          <div className="featured-rooms-center">
+            {loading ? <Loading /> : rooms}
+          </div>
+        </section>
       </div>
     );
   }
---
+++ src/components/Room.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Room() {
+  return (
+    <div>
+      <h1>hello from room component!</h1>
+    </div>
+  );
+}
